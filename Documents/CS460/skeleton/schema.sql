d88f5f70d777374d8d17cb50ad131934f527f0ab
Mhd-Aldahmani <126345161+Mhd-Aldahmani@users.noreply.github.com>
2023-02-28 12:00:48 -0500

    Update schema.sql

--- Documents/CS460/skeleton/schema.sql
+++ Documents/CS460/skeleton/schema.sql
@@ -14,7 +14,14 @@ CREATE TABLE Users (
     hometown varchar(20), 
   CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
-
+CREATE TABLE Friends (
+    usid int4 NOT NULL,
+    fid int4 NOT NULL,
+    CHECK (usid <> fid),
+    PRIMARY KEY(usid, fid),
+    FOREIGN KEY (usid) REFERENCES Users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (fid) REFERENCES Users (user_id) ON DELETE CASCADE
+);
 CREATE TABLE Pictures
 (
   picture_id int4  AUTO_INCREMENT,
